30c55e8747ba0a3c7bd67e7b2023b9e17d80168a
moritz.werr <ticonderoga122@gmail.com>
2024-04-08 21:31:09 +0200

    Hash-Werte für Passwoerter in Datenbank eingerichtet

--- authenticate.php
+++ authenticate.php
@@ -19,7 +19,8 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
 if ($stmt->num_rows > 0) {
 	$stmt->bind_result($id, $password);
 	$stmt->fetch();
-	if ($_POST['password'] === $password) {
+//	if ($_POST['password'] === $password) {
+	if (password_verify($_POST['password'], $password)) {
 		session_regenerate_id();
 		$_SESSION['loggedin'] = TRUE;
 		$_SESSION['name'] = $_POST['username'];
