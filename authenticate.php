77266a6228741a9d695b3d0eb5a22a962a27f8b3
Phil <richter.orag@web.de>
2024-04-18 19:54:30 +0200

    Redirect auf "Mainpage" eingerichtet

--- authenticate.php
+++ authenticate.php
@@ -1,37 +1,50 @@
 <?php
 
-include("access_database.php");
+include ("access_database.php");
+session_name("webeng_mwerr");
 session_start();
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()){
+if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-if ( !isset($_POST['username'], $_POST['password']) ) {
+if (isset($_SESSION['logggedin']) && $_SESSION['loggedin']) {
+	redirect();
+} elseif (isset($_POST['username'], $_POST['password'])) {
+	createSession();
+} else {
 	exit('Please fill both the username and password fields!');
 }
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-	$stmt->bind_param('s', $_POST['username']);
-	$stmt->execute();
-	$stmt->store_result();
-if ($stmt->num_rows > 0) {
-	$stmt->bind_result($id, $password);
-	$stmt->fetch();
-//	if ($_POST['password'] === $password) {
-	if (password_verify($_POST['password'], $password)) {
-		session_regenerate_id();
-		$_SESSION['loggedin'] = TRUE;
-		$_SESSION['name'] = $_POST['username'];
-		$_SESSION['id'] = $id;
-		echo 'Welcome back, ' . htmlspecialchars($_SESSION['name'], ENT_QUOTES) . '!';
-	} else {
-		echo 'Incorrect password!';
-	}
-} else {
-	echo 'Incorrect username!';
+
+function redirect()
+{
+	header('location:mainpage.html');
 }
 
-
-	$stmt->close();
+function createSession()
+{
+	global $con, $id, $password;
+	if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+		$stmt->bind_param('s', $_POST['username']);
+		$stmt->execute();
+		$stmt->store_result();
+		if ($stmt->num_rows > 0) {
+			$stmt->bind_result($id, $password);
+			$stmt->fetch();
+			//	if ($_POST['password'] === $password) {
+			if (password_verify($_POST['password'], $password)) {
+				session_regenerate_id();
+				$_SESSION['loggedin'] = TRUE;
+				$_SESSION['name'] = $_POST['username'];
+				$_SESSION['id'] = $id;
+				redirect();
+			} else {
+				echo 'Incorrect password!';
+			}
+		} else {
+			echo 'Incorrect username!';
+		}
+		$stmt->close();
+	}
 }
 ?>
