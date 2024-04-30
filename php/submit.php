20d779d852c5a32d70d3083ecbc85dbcd96c1f2f
moritz.werr <ticonderoga122@gmail.com>
2024-04-30 16:23:14 +0200

    Bild upload repariert

--- php/submit.php
+++ php/submit.php
@@ -26,31 +26,41 @@ $id_array=$result->fetch_assoc();
 $id = $id_array["id"];
 
 
+
+
+if($filename != NULL){
 $sqlquery = "INSERT INTO threads (id, text, id_account, timestamp, picture_path, title) VALUES (NULL, ?, ?, now(), ?, ?)";
 $stmt = $con->prepare($sqlquery);
 $stmt->bind_param("ssss", $text, $id, $filename , $title );
 
-	echo 'File count=', count($_FILES), "\n";
+	if (move_uploaded_file( $temp_file, $fullpath )){
+		if ($stmt->execute()){
+		    header("Location: ../mainpage.html");
+			
+		}
+		else{
+			echo "ERROR: "
+			. $stmt->error;
+		}
 
-	echo '<pre>';
-	var_dump($_FILES);
-	echo '</pre>';
-	echo "<p>1. $filename 2.  $imagepath 3.  $temp_file</p>";
-
-if (move_uploaded_file( $temp_file, $fullpath )){
-	if ($stmt->execute()){
-	    header("Location: ../mainpage.html");
-		
+	} else {
+		echo "Failed to move your image.\n";	
 	}
-	else{
-		echo "ERROR: "
-		. $stmt->error;
-	}
-
-} else {
-	echo "Failed to move your image.\n";	
-	echo "Not uploaded because of error #".$_FILES['image']['error'];
 }
+else{
 
+$sqlquery = "INSERT INTO threads (id, text, id_account, timestamp, picture_path, title) VALUES (NULL, ?, ?, now(), NULL, ?)";
+$stmt = $con->prepare($sqlquery);
+$stmt->bind_param("sss", $text, $id, $title );
+		if ($stmt->execute()){
+		    header("Location: ../mainpage.html");
+			
+		}
+		else{
+			echo "ERROR: "
+			. $stmt->error;
+		}
+
+}
 
 ?>
