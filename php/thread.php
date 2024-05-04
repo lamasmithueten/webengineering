dbdcb5a50180a7c604bd5f36f874fc84016a0e19
moritz.werr <ticonderoga122@gmail.com>
2024-05-04 11:23:26 +0200

    Threads werden nun angezeigt nach Schema der Hauptseite1

--- php/thread.php
+++ php/thread.php
@@ -1,5 +1,18 @@
 <?php
 	include('php/redirect.php');
+	include('access_database.php');
+	
+	$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+	if (mysqli_connect_errno()){
+		exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+	}
+	$thread_id = $_GET['variable'];
+	$sqlquery = "SELECT username, text, timestamp, picture_path, title, threads.id FROM accounts JOIN threads ON accounts.id = threads.id_account WHERE threads.id = ?";
+	$stmt = $con->prepare($sqlquery);
+	$stmt->bind_param("s", $thread_id);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	$rows=$result->fetch_all(MYSQLI_ASSOC);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,9 +22,32 @@
 </head>
 <body>
 	<p>Thread</p>
-<?php	echo '<pre>';
-	var_dump($_GET);
-	echo '</pre>';
+<?php	
+		foreach ($rows as $fieldname => $arrayEntry){
+			echo "<table>";
+			echo '<tr><td>';
+			echo $arrayEntry['title'];
+			echo "</td></tr><tr>";
+			if (isset($arrayEntry['picture_path'])){
+			$image = $arrayEntry['picture_path'];
+	?>
+			<td>
+			<img src="pictures/<?php echo $image;?>">
+	<?php
+			echo "</td>";
+			}
+			echo "<td>";
+			echo $arrayEntry['text'];
+			echo "</td></tr><tr><td>";
+			echo $arrayEntry['username'];
+			echo "</td><td>";
+			echo $arrayEntry['timestamp'];
+			echo "</td></tr>";
+			echo "</table>";
+			echo "<br>";
+			echo "<br>";
+		}
 ?>
 </body>
 </html>
+<?php mysqli_close($con); ?>
