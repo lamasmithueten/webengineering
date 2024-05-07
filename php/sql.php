d82c2c1c12d48095a4c43741f719f7f02a6ef00b
moritz.werr <ticonderoga122@gmail.com>
2024-05-07 16:56:47 +0200

    Threadseite umgebaut zu php Funktionen

--- php/sql.php
+++ php/sql.php
@@ -10,7 +10,7 @@ function openConnection(){
 }
 
 
-function fetchThreads($con){
+function fetchThreadsMainpage($con){
 	$sqlquery = "SELECT username, text, timestamp, picture_path, title, threads.id FROM accounts JOIN threads ON accounts.id = threads.id_account order by timestamp DESC";
 	$stmt = $con->prepare($sqlquery);
 	$stmt->execute();
@@ -23,4 +23,27 @@ function closeConnection($con){
 	mysqli_close($con);
 }
 
+function fetchThread($con, $thread_id){
+	$sqlquery = "SELECT username, text, timestamp, picture_path, title, threads.id FROM accounts JOIN threads ON accounts.id = threads.id_account WHERE threads.id = ?";
+	$stmt = $con->prepare($sqlquery);
+	$stmt->bind_param("s", $thread_id);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	if ($result ->num_rows==0){
+		header("Location: https://webeng.mwerr.de/");
+		exit;
+	}
+	$rows=$result->fetch_all(MYSQLI_ASSOC);
+	return $rows;
+}
+
+function fetchAllComments($con, $thread_id){
+		$sqlquery = "select accounts.username, comments.text, comments.timestamp from accounts join comments on accounts.id=comments.id_account join threads on comments.id_thread=threads.id where threads.id=?";
+		$stmt = $con->prepare($sqlquery);
+		$stmt->bind_param("s", $thread_id);
+		$stmt->execute();
+		$result = $stmt -> get_result();
+		$rows=$result->fetch_all(MYSQLI_ASSOC);
+		return $rows;
+}
 ?>
--- php/thread.php
+++ php/thread.php
@@ -1,21 +1,13 @@
 <?php
 	include('redirect.php');
 	include("sql.php");
+	include("drawsite.php");
 
 	$userID = $_SESSION['id'];
+	$thread_id = $_GET['variable'];
 	
 	$con = openConnection();
-	$thread_id = $_GET['variable'];
-	$sqlquery = "SELECT username, text, timestamp, picture_path, title, threads.id FROM accounts JOIN threads ON accounts.id = threads.id_account WHERE threads.id = ?";
-	$stmt = $con->prepare($sqlquery);
-	$stmt->bind_param("s", $thread_id);
-	$stmt->execute();
-	$result = $stmt->get_result();
-	if ($result ->num_rows==0){
-		header("Location: https://webeng.mwerr.de/");
-		exit;
-	}
-	$rows=$result->fetch_all(MYSQLI_ASSOC);
+	$rows=fetchThread($con, $thread_id);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,51 +19,11 @@
 <body>
 	<p>Thread</p>
 <?php	
-foreach ($rows as $fieldname => $arrayEntry) {
-	echo '<div class="thread">';
-	echo '<div class="title">' . str_replace(array('\r\n', '\n\r', '\n', '\r'), '<br>', $arrayEntry['title']) . '</div>';
-	echo '<div class="post">';
-	if (isset($arrayEntry['picture_path'])) {
-		$image = $arrayEntry['picture_path'];
-		echo '<div class="image"><img src="https://webeng.mwerr.de/pictures/' . $image . '"></div>';
-	}
-	echo '<div class="text">' . str_replace(array('\r\n', '\n\r', '\n', '\r'), '<br>', $arrayEntry['text']) . '</div>';
-	echo '</div>'; 
-	echo '<div class="user_info">';
-	echo '<div class="username">' . $arrayEntry['username'] . '</div>';
-	echo '<div class="timestamp">' . $arrayEntry['timestamp'] . '</div>';
-	echo '</div>'; 
-	echo '</div>'; 
-}
-
-echo '<div class="comment-form">';
-echo '<p>Submit a comment</p>';
-echo '<form action="https://webeng.mwerr.de/php/submit_comment.php" method="post" enctype="multipart/form-data">';
-echo '<textarea id="comment" name="comment" maxlength="65535" required></textarea>';
-echo '<input type="hidden" name="threadid" id="threadid" value="' . $thread_id . '"/>'; 
-echo '<input type="submit" value="submit"/>';
-echo '</form>';
-echo '</div>';
-		$sqlquery = "select accounts.username, comments.text, comments.timestamp from accounts join comments on accounts.id=comments.id_account join threads on comments.id_thread=threads.id where threads.id=?";
-		$stmt = $con->prepare($sqlquery);
-		$stmt->bind_param("s", $thread_id);
-		$stmt->execute();
-		$result = $stmt -> get_result();
-		$rows=$result->fetch_all(MYSQLI_ASSOC);
-
-echo '<div class="comments">';
-echo '<p>Comments</p>';
-foreach ($rows as $fieldname => $arrayEntry) {
-	echo '<div class="comment">';
-	echo '<div class="comment-text">' . str_replace(array('\r\n', '\n\r', '\n', '\r'), '<br>', $arrayEntry['text']) . '</div>';
-	echo '<div class="comment-info">';
-	echo '<span class="comment-username">' . $arrayEntry['username'] . '</span>';
-	echo '<span class="comment-timestamp">' . $arrayEntry['timestamp'] . '</span>';
-	echo '</div>'; 
-	echo '</div>'; 
-}
-echo '</div>'; 
-	?>
+drawThread($rows);
+drawSubmitComments($thread_id);
+$rows=fetchAllComments($con, $thread_id);
+drawComments($rows);
+?>
 </body>
 </html>
 <?php closeConnection($con); ?>
