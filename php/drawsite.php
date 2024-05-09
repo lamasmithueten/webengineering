265d592f0f885464954887e772e8bfc86a8d1216
Phil <richter.orag@web.de>
2024-05-09 15:41:35 +0200

    mainpage.css in style.css integriert

--- php/drawsite.php
+++ php/drawsite.php
@@ -1,62 +1,65 @@
 <?php
 
-include("sql.php");
+include ("sql.php");
 
-function drawThreadsMainpage(){
+function drawThreadsMainpage()
+{
 	$con = openConnection();
-	$rows=fetchThreadsMainpage($con);
+	$rows = fetchThreadsMainpage($con);
 	closeConnection($con);
 	foreach ($rows as $fieldname => $arrayEntry) {
 		echo '<div class="thread">';
 		echo '<div class="title"><a href="https://webeng.mwerr.de/thread/' .
-		$arrayEntry["id"] .
-		'">';
+			$arrayEntry["id"] .
+			'">';
 		echo str_replace(
-		['\r\n', '\n\r', '\n', '\r'],
-		"<br>",
-		$arrayEntry["title"]
+			['\r\n', '\n\r', '\n', '\r'],
+			"<br>",
+			$arrayEntry["title"]
 		);
 		echo "</a></div>";
 
 		echo '<div class="post">';
 		if (isset($arrayEntry["picture_path"])) {
-		$image = $arrayEntry["picture_path"];
-		echo '<div class="image"><img src="https://webeng.mwerr.de/pictures/' .
-		    $image .
-		    '"></div>';
+			$image = $arrayEntry["picture_path"];
+			echo '<div class="image"><img src="https://webeng.mwerr.de/pictures/' .
+				$image .
+				'"></div>';
 		}
 		echo '<div class="text">' .
-		str_replace(
-		    ['\r\n', '\n\r', '\n', '\r'],
-		    "<br>",
-		    $arrayEntry["text"]
-		) .
-		"</div>";
-		echo "</div>"; 
+			str_replace(
+				['\r\n', '\n\r', '\n', '\r'],
+				"<br>",
+				$arrayEntry["text"]
+			) .
+			"</div>";
+		echo "</div>";
 
 		echo '<div class="user_info">';
 		echo '<div class="username">' . $arrayEntry["username"] . "</div>";
 		echo '<div class="timestamp">' .
-		$arrayEntry["timestamp"] .
-		"</div>";
-		echo "</div>"; 
+			$arrayEntry["timestamp"] .
+			"</div>";
+		echo "</div>";
 
-		echo "</div>"; 
+		echo "</div>";
 		echo "<br>";
 	}
 }
 
-function drawThreadsPage($thread_id){
+function drawThreadsPage($thread_id)
+{
 	$con = openConnection();
-	$rows=fetchThread($con, $thread_id);
+	$rows = fetchThread($con, $thread_id);
 	drawThread($rows);
 	drawSubmitComments($thread_id);
-	$rows=fetchAllComments($con, $thread_id);
-	closeConnection($con); 
+	$rows = fetchAllComments($con, $thread_id);
+	closeConnection($con);
 	drawComments($rows);
 }
 
-function drawThread($rows){
+function drawThread($rows)
+{
 	foreach ($rows as $fieldname => $arrayEntry) {
 		echo '<div class="thread">';
 		echo '<div class="title">' . str_replace(array('\r\n', '\n\r', '\n', '\r'), '<br>', $arrayEntry['title']) . '</div>';
@@ -66,39 +69,41 @@ function drawThread($rows){
 			echo '<div class="image"><img src="https://webeng.mwerr.de/pictures/' . $image . '"></div>';
 		}
 		echo '<div class="text">' . str_replace(array('\r\n', '\n\r', '\n', '\r'), '<br>', $arrayEntry['text']) . '</div>';
-		echo '</div>'; 
+		echo '</div>';
 		echo '<div class="user_info">';
 		echo '<div class="username">' . $arrayEntry['username'] . '</div>';
 		echo '<div class="timestamp">' . $arrayEntry['timestamp'] . '</div>';
-		echo '</div>'; 
-		echo '</div>'; 
+		echo '</div>';
+		echo '</div>';
 	}
 
 }
-function drawSubmitComments ($thread_id){
+function drawSubmitComments($thread_id)
+{
 	echo '<div class="comment-form">';
 	echo '<p>Submit a comment</p>';
 	echo '<form action="https://webeng.mwerr.de/php/submit_comment.php" method="post" enctype="multipart/form-data">';
 	echo '<textarea id="comment" name="comment" maxlength="65535" required></textarea>';
-	echo '<input type="hidden" name="threadid" id="threadid" value="' . $thread_id . '"/>'; 
+	echo '<input type="hidden" name="threadid" id="threadid" value="' . $thread_id . '"/>';
 	echo '<input type="submit" value="submit"/>';
 	echo '</form>';
 	echo '</div>';
 }
 
-function drawComments($rows){
-echo '<div class="comments">';
-echo '<p>Comments</p>';
+function drawComments($rows)
+{
+	echo '<div class="comments">';
+	echo '<p>Comments</p>';
 	foreach ($rows as $fieldname => $arrayEntry) {
 		echo '<div class="comment">';
 		echo '<div class="comment-text">' . str_replace(array('\r\n', '\n\r', '\n', '\r'), '<br>', $arrayEntry['text']) . '</div>';
 		echo '<div class="comment-info">';
 		echo '<span class="comment-username">' . $arrayEntry['username'] . '</span>';
 		echo '<span class="comment-timestamp">' . $arrayEntry['timestamp'] . '</span>';
-		echo '</div>'; 
-		echo '</div>'; 
+		echo '</div>';
+		echo '</div>';
 	}
-	echo '</div>'; 
+	echo '</div>';
 }
 
 ?>
--- php/sql.php
+++ php/sql.php
@@ -1,75 +1,82 @@
 <?php
 
-function openConnection(){
-	include("access_database.php");
+function openConnection()
+{
+	include ("access_database.php");
 	$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-	if (mysqli_connect_errno()){
+	if (mysqli_connect_errno()) {
 		exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 	}
 	return $con;
 }
 
 
-function fetchThreadsMainpage($con){
+function fetchThreadsMainpage($con)
+{
 	$sqlquery = "SELECT username, text, timestamp, picture_path, title, threads.id FROM accounts JOIN threads ON accounts.id = threads.id_account order by timestamp DESC";
 	$stmt = $con->prepare($sqlquery);
 	$stmt->execute();
-	$result = $stmt -> get_result();
+	$result = $stmt->get_result();
 	$rows = $result->fetch_all(MYSQLI_ASSOC);
 	return $rows;
 }
 
-function closeConnection($con){
+function closeConnection($con)
+{
 	mysqli_close($con);
 }
 
-function fetchThread($con, $thread_id){
+function fetchThread($con, $thread_id)
+{
 	$sqlquery = "SELECT username, text, timestamp, picture_path, title, threads.id FROM accounts JOIN threads ON accounts.id = threads.id_account WHERE threads.id = ?";
 	$stmt = $con->prepare($sqlquery);
 	$stmt->bind_param("s", $thread_id);
 	$stmt->execute();
 	$result = $stmt->get_result();
-	if ($result ->num_rows==0){
+	if ($result->num_rows == 0) {
 		header("Location: https://webeng.mwerr.de/");
 		exit;
 	}
-	$rows=$result->fetch_all(MYSQLI_ASSOC);
+	$rows = $result->fetch_all(MYSQLI_ASSOC);
 	return $rows;
 }
 
-function fetchAllComments($con, $thread_id){
-		$sqlquery = "select accounts.username, comments.text, comments.timestamp from accounts join comments on accounts.id=comments.id_account join threads on comments.id_thread=threads.id where threads.id=?";
-		$stmt = $con->prepare($sqlquery);
-		$stmt->bind_param("s", $thread_id);
-		$stmt->execute();
-		$result = $stmt -> get_result();
-		$rows=$result->fetch_all(MYSQLI_ASSOC);
-		return $rows;
+function fetchAllComments($con, $thread_id)
+{
+	$sqlquery = "select accounts.username, comments.text, comments.timestamp from accounts join comments on accounts.id=comments.id_account join threads on comments.id_thread=threads.id where threads.id=?";
+	$stmt = $con->prepare($sqlquery);
+	$stmt->bind_param("s", $thread_id);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	$rows = $result->fetch_all(MYSQLI_ASSOC);
+	return $rows;
 }
 
-function checkUsernameTaken($con, $username){
+function checkUsernameTaken($con, $username)
+{
 	$sqlquery = "SELECT username FROM accounts WHERE username = ?";
 	$stmt = $con->prepare($sqlquery);
 	$stmt->bind_param("s", $username);
 	$stmt->execute();
-	$result = $stmt -> get_result();
+	$result = $stmt->get_result();
 
-	if ($result ->num_rows>0){
+	if ($result->num_rows > 0) {
 		echo "Username $username ist schon in Verwendung.";
 		echo '<br><a href="../register">Zurück zum Registrieren</a><br><a href="../index">Zurück zur Anmeldung</a><br>';
 		exit();
 	}
 }
 
-function checkEmailTaken($con, $email){
+function checkEmailTaken($con, $email)
+{
 	$sqlquery = "SELECT email FROM accounts WHERE email = ?";
 	$stmt = $con->prepare($sqlquery);
 	$stmt->bind_param("s", $email);
 	$stmt->execute();
-	$result = $stmt -> get_result();
+	$result = $stmt->get_result();
 
 
-	if ($result ->num_rows>0){
+	if ($result->num_rows > 0) {
 		echo "Emailadresse $email ist schon in Verwendung.";
 		echo '<br><a href="../register">Zurück zum Registrieren</a><br><a href="index">Zurück zur Anmeldung</a><br>';
 		exit();
@@ -77,72 +84,72 @@ function checkEmailTaken($con, $email){
 }
 
 
-function createUser($con, $username, $password, $email){
-	$sqlquery = "INSERT INTO accounts (id, username, password, email) VALUES ( NULL, ?, ?, ?)" ;
+function createUser($con, $username, $password, $email)
+{
+	$sqlquery = "INSERT INTO accounts (id, username, password, email) VALUES ( NULL, ?, ?, ?)";
 	$stmt = $con->prepare($sqlquery);
 	$stmt->bind_param("sss", $username, $password, $email);
 
 
-	if($stmt->execute()){
+	if ($stmt->execute()) {
 		$text = "Dein Account $username wurde erfolgreich erstellt.\n";
-		mail ($email, "Account aktiviert", $text );
+		mail($email, "Account aktiviert", $text);
 		echo "<h3>Account registriert<h3>";
-		echo nl2br ("\n$username\n $email\n ");
+		echo nl2br("\n$username\n $email\n ");
 		echo '<br><a href="index">Zurück zur Anmeldung</a><br>';
-	} 
-	else {
+	} else {
 		echo "ERROR: "
-		. $stmt->error;
+			. $stmt->error;
 	}
 }
 
-function createComment($con, $thread_id, $comment, $account_id){
+function createComment($con, $thread_id, $comment, $account_id)
+{
 	$sqlquery = "INSERT INTO comments (id, id_thread, text, timestamp, id_account) VALUES (NULL, ?, ?, now(), ?)";
 	$stmt = $con->prepare($sqlquery);
 	$stmt->bind_param("isi", $thread_id, $comment, $account_id);
-	if($stmt->execute()){
+	if ($stmt->execute()) {
 		header("Location: https://webeng.mwerr.de/thread/$thread_id");
 		exit;
-	}
-	else{
+	} else {
 		echo "ERROR: "
-		. $stmt->error;
+			. $stmt->error;
 	}
 	$stmt->close();
 }
 
-function submitThreadWithPicture($con, $text, $id, $filename, $title, $temp_file, $fullpath ){
+function submitThreadWithPicture($con, $text, $id, $filename, $title, $temp_file, $fullpath)
+{
 	$sqlquery = "INSERT INTO threads (id, text, id_account, timestamp, picture_path, title) VALUES (NULL, ?, ?, now(), ?, ?)";
 	$stmt = $con->prepare($sqlquery);
-	$stmt->bind_param("siss", $text, $id, $filename , $title );
+	$stmt->bind_param("siss", $text, $id, $filename, $title);
 
-		if (move_uploaded_file( $temp_file, $fullpath )){
-			if ($stmt->execute()){
-			    header("Location: ../mainpage.html");
-				
-			}
-			else{
-				echo "ERROR: "
-				. $stmt->error;
-			}
+	if (move_uploaded_file($temp_file, $fullpath)) {
+		if ($stmt->execute()) {
+			header("Location: ../mainpage.html");
 
 		} else {
-			echo "Failed to move your image.\n";	
+			echo "ERROR: "
+				. $stmt->error;
 		}
+
+	} else {
+		echo "Failed to move your image.\n";
+	}
 }
 
-function submitThreadWithoutPicture($con, $text, $id, $title){
+function submitThreadWithoutPicture($con, $text, $id, $title)
+{
 	$sqlquery = "INSERT INTO threads (id, text, id_account, timestamp, picture_path, title) VALUES (NULL, ?, ?, now(), NULL, ?)";
 	$stmt = $con->prepare($sqlquery);
-	$stmt->bind_param("sss", $text, $id, $title );
-		if ($stmt->execute()){
-		    header("Location: ../mainpage.html");
-			
-		}
-		else{
-			echo "ERROR: "
+	$stmt->bind_param("sss", $text, $id, $title);
+	if ($stmt->execute()) {
+		header("Location: ../mainpage.html");
+
+	} else {
+		echo "ERROR: "
 			. $stmt->error;
-		}
+	}
 }
 
 ?>
