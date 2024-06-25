fd888c895f010cdd09a7622632954e7ecd1ba0b7
moritz.werr <ticonderoga122@gmail.com>
2024-06-25 09:28:52 +0200

    SQL-Funktionen fuer Liken Thread erstellt, neue PHP-Datei fuer Threadlike erstellt

--- php/like_thread.php
+++ php/like_thread.php
@@ -4,18 +4,18 @@ include("redirect.php");
 include("sql.php");
 
 $con = openConnection();
-$id_comment = $_POST['comment_id'];
+$id_thread = $_POST['thread_id'];
 $id_user = $_SESSION['id'];
 $action = $_POST['action'];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	if ($action === 'like') {
-		createLikeComment($con, $id_user, $id_comment);
+		createLikeThread($con, $id_user, $id_thread);
 	}
 	else if ($action === 'unlike') {
-		deleteLikeComment($con, $id_user, $id_comment);
+		deleteLikeThread($con, $id_user, $id_thread);
 	}
-	$like_count = getLikeCountComment($con, $id_comment);
+	$like_count = getLikeCountThread($con, $id_thread);
 	echo json_encode(['like_count' => $like_count]);	
 }
 
